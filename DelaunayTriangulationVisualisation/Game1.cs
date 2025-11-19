1554a19cc42e3d0648a8a7b20fd0b950ea7b741b
FintanTheProgrammer <bishopfintan1@gmail.com>
2025-11-19 14:09:35 +0000

    Add project files.

---
+++ DelaunayTriangulationVisualisation/Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DelaunayTriangulationVisualisation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Color BackgroundColor = new(0, 127, 255);
+
+        public static readonly (int Width, int Height) Resolution = (1280, 720);
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = Resolution.Width,
+                PreferredBackBufferHeight = Resolution.Height
+            };
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(BackgroundColor);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ DelaunayTriangulationVisualisation/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new DelaunayTriangulationVisualisation.Game1();
+game.Run();
